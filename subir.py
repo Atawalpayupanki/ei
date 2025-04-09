39a49e5a5129aa8ee8c716dadb95e2e50eafc44c
Pau <pautall667@gmail.com>
2025-04-09 18:33:11 +0200

    secondcommit

--- subir.py
+++ subir.py
@@ -4,7 +4,7 @@ comandos= [
     ["git", "add", "."],
     ["git", "commit", "-m", "secondcommit"],
     ["git", "branch", "-M", "main"],
-    ["git", "push", "--force", "origin", "main"],
+    ["git", "push", "origin", "main"],
     ]
 for comando in comandos:
     subprocess.run(comando, capture_output=True, text=True)
